7700694fbdc6aaed310366bdc89449d9dae82a87
maria <mariacodes77@gmail.com>
2025-12-16 11:23:02 +0300

    feat(car): use NamedQuery to get cars by owner

--- city-residents-service/src/main/java/com/example/cityresidents/repo/CarRepository.java
+++ city-residents-service/src/main/java/com/example/cityresidents/repo/CarRepository.java
@@ -2,8 +2,13 @@ package com.example.cityresidents.repo;
 
 import com.example.cityresidents.domain.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
-    List<Car> findByOwnerId(Long ownerId);
+
+    @Query(name = "Car.findByOwnerId")
+    List<Car> findByOwnerId(@Param("ownerId") Long ownerId);
 }
