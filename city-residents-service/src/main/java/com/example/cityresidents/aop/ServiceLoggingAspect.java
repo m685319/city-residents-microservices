fb30a0ddf93470dad089737ca74e45230d24dd26
maria <mariacodes77@gmail.com>
2026-01-01 01:54:34 +0300

    chore(aop): define execution order for service aspects

--- city-residents-service/src/main/java/com/example/cityresidents/aop/ServiceLoggingAspect.java
+++ city-residents-service/src/main/java/com/example/cityresidents/aop/ServiceLoggingAspect.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
+@Order(1)
 @Slf4j
 @Aspect
 @Component
--- city-residents-service/src/main/java/com/example/cityresidents/aop/ServiceTimingAspect.java
+++ city-residents-service/src/main/java/com/example/cityresidents/aop/ServiceTimingAspect.java
@@ -4,8 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
+@Order(2)
 @Slf4j
 @Aspect
 @Component
